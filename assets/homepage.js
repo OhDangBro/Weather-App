3e2f2c1a76742a87e7893904ca90ac02e3694190
Will <103394466+OhDangBro@users.noreply.github.com>
2022-06-15 11:13:52 -0400

    redid layout, added dates, temp and icons to five day forecast, restructed one day forcast

--- assets/homepage.js
+++ assets/homepage.js
@@ -12,6 +12,11 @@ var dayTwo = document.querySelector(".dayTwo");
 var dayThree = document.querySelector(".dayThree");
 var dayFour = document.querySelector(".dayFour");
 var dayFive = document.querySelector(".dayFive");
+var cityValues = []
+
+
+
+
 
 
 
@@ -23,13 +28,21 @@ function showDate() {
 
 }
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function (event) {
+    console.log(event)
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" +  inputValue.value +"&units=imperial&appid=e04fbf348ab68f01c8da11552c3c99df")
         .then(response => response.json())
       
 
         .then(data => {
-            console.log(data);
+            // console.log(data);
+            // console.log(inputValue.value);
+            cityValues.push(inputValue.value);
+            console.log(cityValues)
+            localStorage.setItem("cityKey", cityValues);
+            $(".list-groupss").append(`<li class="list-group-item">${inputValue.value}</li> `);
+
+
             var nameValue = data['name'];
             var tempValue = data['main']['temp'];
             var windIndexValue = data.wind.speed;
@@ -38,11 +51,11 @@ button.addEventListener("click", function () {
             latitude = data.coord.lat;
             longitude = data.coord.lon;
 
-            cityVal.innerHTML = nameValue;
-            temp.innerHTML = "The Current Temp is : " + tempValue;
-            description.innerHTML = "Its : " + descValue;
-            humidity.innerHTML = "The Humidity Level is : " + humidityValue;
-            windIndex.innerHTML = "The wind value is : " + windIndexValue;
+            cityVal.innerHTML = "Currently in" + nameValue + ":";
+            temp.innerHTML = "with a current temp of  : " + tempValue;
+            description.innerHTML = "were seeing " + descValue;
+            humidity.innerHTML = "with a humidity level of : " + humidityValue;
+            windIndex.innerHTML = "and a wind speed of: " + windIndexValue;
             // console.log(nameValue);
             // console.log(tempValue);
             // console.log(descValue);
@@ -56,24 +69,67 @@ button.addEventListener("click", function () {
 });
 
 function fiveDayForecast(lat,long) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&cnt=10&units=imperial&appid=e04fbf348ab68f01c8da11552c3c99df")
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&cnt=100&units=imperial&appid=e04fbf348ab68f01c8da11552c3c99df")
     .then(response => response.json())
+    // console.log(data)
     .then(data => {
+        console.log(data)
+
+        var dayOneValue = data.daily[0].temp.day;
+        var dayTwoValue = data.daily[1].temp.day;
+        var dayThreeValue = data.daily[2].temp.day;
+        var dayFourValue = data.daily[3].temp.day;
+        var dayFiveValue = data.daily[4].temp.day;
+        
         
 
-        var dayOneValue = data.daily[0].temp.day
-        var dayTwoValue = data.daily[1].temp.day
-        var dayThreeValue = data.daily[2].temp.day
-        var dayFourValue = data.daily[3].temp.day
-        var dayFiveValue = data.daily[4].temp.day
-
-        dayOne.innerHTML = dayOneValue;
+            dayOne.innerHTML = dayOneValue;
             dayTwo.innerHTML = dayTwoValue;
             dayThree.innerHTML = dayThreeValue;
             dayFour.innerHTML = dayFourValue;
             dayFive.innerHTML = dayFiveValue;
+            var iconcode = data.daily[0].weather[0].icon;
+            var iconcodeTwo = data.daily[1].weather[0].icon;
+            var iconcodeThree = data.daily[2].weather[0].icon;
+            var iconcodeFour = data.daily[3].weather[0].icon;
+            var iconcodeFive = data.daily[4].weather[0].icon;
+            var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            var iconUrlTwo = "http://openweathermap.org/img/w/" + iconcodeTwo + ".png";
+            var iconUrlThree = "http://openweathermap.org/img/w/" + iconcodeThree + ".png";
+            var iconUrlFour = "http://openweathermap.org/img/w/" + iconcodeFour + ".png";
+            var iconUrlFive = "http://openweathermap.org/img/w/" + iconcodeFive + ".png";
+            var dateOneValue = moment().format("MMM Do YYYY");
+            dateOne.innerHTML = dateOneValue;
+
+            var dateTwoValue = moment().add(1, 'days').calendar(); ;     
+            dateTwo.innerHTML = dateTwoValue;
+           
+            var dateThreeValue = moment().add(2, 'days').calendar(); ;     
+            dateThree.innerHTML = dateThreeValue;
+
+            var dateFourValue = moment().add(3, 'days').calendar(); ;     
+            dateFour.innerHTML = dateFourValue;
+
+            var dateFiveValue = moment().add(4, 'days').calendar(); ;     
+            dateFive.innerHTML = dateFiveValue;
+
+
+
+
+            $('#wicon').attr('src', iconUrl);
+            $('#wiconTwo').attr('src', iconUrlTwo);
+            $('#wiconThree').attr('src', iconUrlThree);
+            $('#wiconFour').attr('src', iconUrlFour);
+            $('#wiconFive').attr('src', iconUrlFive);
+           
+            
+
+
+            
 
     });
     console.log(dayOneValue)
     console.log(dayTwoValue)
+
 }
+
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 
-var inputValue = document.querySelector("#username");
-var name = document.querySelector(".name");
-var description = document.querySelector(".description");
-var temp = document.querySelector(".temp");
-var windIndex = document.querySelector(".wind");
-var humidity = document.querySelector(".humidity");
+// var inputValue = document.querySelector("#username");
+// var name = document.querySelector(".name");
+// var description = document.querySelector(".description");
+// var temp = document.querySelector(".temp");
+// var windIndex = document.querySelector(".wind");
+// var humidity = document.querySelector(".humidity");
 
